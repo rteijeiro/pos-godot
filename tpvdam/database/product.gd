48e32a9c955d98dead3f08f714aca50ee17c404b
Bolgeg <ricardobolgeg@gmail.com>
2025-04-01 12:02:18 +0200

    Database classes

---
+++ tpvdam/database/product.gd
@@ -0,0 +1,10 @@
+class_name Product
+extends Object
+
+enum categories { STARTERS, MEAT, FISH, PASTA, DESSERTS, DRINKS }
+
+var id:int=0
+var name:String=""
+var image:String=""
+var price:float=0.0
+var category:int=categories.STARTERS
---
+++ tpvdam/database/waiter.gd
@@ -0,0 +1,7 @@
+class_name Waiter
+extends Object
+
+var id:int=0
+var name:String=""
+var surname:String=""
+var image:String=""
