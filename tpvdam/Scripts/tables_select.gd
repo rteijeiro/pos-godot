cc5bea38c210709a364295473de9b00c8b5c9283
Álvaro <alvarocc948@gmail.com>
2025-04-01 16:07:23 +0200

    Updating the scene and button call methods

---
+++ tpvdam/Scripts/tables_select.gd
@@ -0,0 +1,66 @@
+extends Node2D
+
+# ---------- Calling methods for table buttons ----------
+func _on_button_table_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_table_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_table_3_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_table_4_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_table_5_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_table_6_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_table_7_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_table_8_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_table_9_pressed() -> void:
+	pass # Replace with function body.
+
+
+# ---------- Stool button calling methods ----------
+func _on_button_stool_10_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_stool_11_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_stool_12_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_stool_13_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_stool_14_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_stool_15_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_stool_16_pressed() -> void:
+	pass # Replace with function body.
