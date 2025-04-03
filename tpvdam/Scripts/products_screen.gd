464151291b05d7d3f19ccbbffcd9ff0785e0bac0
Álvaro <alvarocc948@gmail.com>
2025-04-03 13:37:39 +0200

    Connecting the interface buttons

---
+++ tpvdam/Scripts/products_screen.gd
@@ -0,0 +1,66 @@
+extends Node2D
+
+# ---------- FTypes of food ----------
+func _on_button_starters_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_meat_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_fish_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_pasta_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_dessert_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_drinks_pressed() -> void:
+	pass # Replace with function body.
+
+
+# ---------- Others buttons ----------
+func _on_button_print_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_pay_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_delete_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_add_notes_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_tickets_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_open_box_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_change_table_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/tablesSelectScreen.tscn")
+
+
+func _on_button_split_ticket_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_pays_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_logout_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
