ad6de8a8a30d418a39a04c9157132b21c3034095
Flapx24 <bares.gafer23@cadiz.salesianos.edu>
2025-04-04 10:50:50 +0200

    Add new general functions in globals script

--- tpvdam/Scripts/globals.gd
+++ tpvdam/Scripts/globals.gd
@@ -1,27 +1,47 @@
 extends Node
 
-var selected_table_id:=0
-var logged_waiter_id:=0
+var logged_waiter:Waiter
 
+var selected_table:Table
+
+#Tables functions
 func get_selected_table():
-	return Database.tables[selected_table_id-1]
+	return selected_table
+
+func set_selected_table(table_id:int):
+	selected_table = Database.tables[table_id-1]
 
 func add_product_to_selected_table(product_id:int):
-	Database.tables[selected_table_id-1].products.append(product_id)
-	Database.tables[selected_table_id-1].status=Table.statuses.OCCUPIED
+	selected_table.products.append(product_id)
+	selected_table.status=Table.statuses.OCCUPIED
+
+func clear_selected_table():
+	selected_table = null
 
 func get_selected_table_amount() -> float:
 	var sum:=0.0
-	for product_id in Database.tables[selected_table_id-1].products:
+	for product_id in selected_table.products:
 		sum+=get_product_from_id(product_id).price
 	return sum
 
+#Waiters functions
 func get_logged_waiter():
+	if logged_waiter!=null:
+		return logged_waiter
+	else:
+		return null
+	
+func set_logged_waiter(waiter_id:int):
 	for waiter in Database.waiters:
-		if waiter.id==logged_waiter_id:
-			return waiter
-	assert(false,"Waiter not logged in")
+		if waiter.id==waiter_id:
+			logged_waiter = waiter
+			return
+	logged_waiter = null
+	
+func delete_waiter(waiter_id:int):
+	Database.waiters.remove_at(waiter_id-1)
 
+#Products functions
 func get_product_from_id(product_id:int):
 	for product in Database.products:
 		if product.id==product_id:
